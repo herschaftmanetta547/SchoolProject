14f0014b1c63138ce929a3e698f43209283f0ca5
herschaftmanetta547 <herschaftmanetta547@gmail.com>
2025-03-13 10:45:35 +0000

    Add 7.cpp

---
+++ src/7.cpp
@@ -0,0 +1,12 @@
+
+// A simple C++ program that calculates the sum of two numbers
+
+#include <iostream>
+
+int main() {
+   int num1 = 5;
+   int num2 = 8;
+   int sum = num1 + num2;
+   std::cout << "The sum of " << num1 << " and " << num2 << " is " << sum << "\n";
+   return 0;
+}
