7263d47a073b581de2dbc79f1ceea1303493dd9e
herschaftmanetta547 <herschaftmanetta547@gmail.com>
2025-03-22 15:51:16 +0000

    Add 16.cpp

---
+++ src/16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 3;
+    
+    cout << "The value of x is: " << x << endl;
+    cout << "The value of y is: " << y << endl;
+    
+    if (x > y) {
+        cout << "x is greater than y." << endl;
+    } else {
+        cout << "y is greater than x." << endl;
+    }
+    
+    return 0;
+}
